d0b05eeddedf7ff9cf7b9e92b0162647dd86ddc3
Soofiaa <soofiaa.menzel@gmail.com>
2022-09-19 16:47:59 -0300

    creación header y footer
    cree la cabecera y el pie de pagina, si bien los estilos del pie hay que cambiarlo, no sé cómo implementar 'modularmente' a cada página los elementos creados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './paginas/inicio/inicio.component';
 import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { GaleriaComponent } from './paginas/galeria/galeria.component';
+import { IngresoAulaVirtualComponent } from './paginas/ingreso-aula-virtual/ingreso-aula-virtual.component';
+import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
+import { PerfilAulaVirtualComponent } from './paginas/perfil-aula-virtual/perfil-aula-virtual.component';
+import { Asignatura1Component } from './paginas/perfilAulaVirtual/asignatura1/asignatura1.component';
 
+/* rutas para cada pagina del sitio web*/
 const routes: Routes = [
-  {path: '', component: InicioComponent}, 
-  {path: 'contacto', component: ContactoComponent}, 
+  {path: '', component: InicioComponent },
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'galeria', component: GaleriaComponent},
+  {path: 'ingreso-aula-virtual', component: IngresoAulaVirtualComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'perfil-aula-virtual', component: PerfilAulaVirtualComponent},
+  {path: 'perfil-aula-virtual/asignatura1', component: Asignatura1Component}, 
 ];
 
 @NgModule({
