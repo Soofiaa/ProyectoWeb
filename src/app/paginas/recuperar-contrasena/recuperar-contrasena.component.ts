57b6562b9bf7e37cf296c97334058775c83876b1
Soofiaa <soofiaa.menzel@gmail.com>
2022-09-30 20:34:08 -0300

    30-09 20:35 APROX
    -ARREGLÉ UNO QUE OTRO FORMULARIO REACTIVO PERO TIENEN ERRORES POR CONSOLA CUANDO LO EJECUTAS EN EL NAVEGADOR
    -AVANZANDO INTERFAZ PERFIL ALUMNO, SÓLO LLEVO LA IMAGEN... NECESITO CENTRARLA

--- src/app/paginas/recuperar-contrasena/recuperar-contrasena.component.ts
+++ src/app/paginas/recuperar-contrasena/recuperar-contrasena.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recuperar-contrasena',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recuperar-contrasena.component.scss']
 })
 export class RecuperarContrasenaComponent implements OnInit {
+  resultado!: string;
+  formularioRecuperarContrasena: FormGroup = {} as FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submit() {
+    if (this.formularioRecuperarContrasena.valid)
+      this.resultado = "Todos los datos son válidos";
+    else
+      this.resultado = "Hay datos inválidos en el formulario";
   }
-
+  ngOnInit():void{
+    this.formularioRecuperarContrasena = new FormGroup({
+    correo: new FormControl('', [Validators.required]),
+    });
+  }
 }
